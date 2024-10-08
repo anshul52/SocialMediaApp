36773fa9462bb29d45d9f3ad4801e97e7f1c1f27
AnshulGupta <tech.anshul@subhx.in>
2024-10-09 00:59:28 +0530

    login

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/auth.controller");
+const { createUser, loginUser } = require("../controllers/auth.controller");
 router.post("/createUser", createUser);
+router.post("/loginUser", loginUser);
 
 module.exports = router;
